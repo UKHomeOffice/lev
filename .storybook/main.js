a689df546241f082e9fa3d35ba59a5ac6072a4ef
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-08-27 14:15:21 +0100

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  stories: [
+    '../components/*/spec/*.stories.@(js|mdx)',
+    '../styles/*.stories.mdx',
+    '../packages/components/src/components/**/*.stories.mdx'
+  ],
+  addons: [
+    {
+      name: '@not-govuk/storybook-preset',
+      options: {
+        baseDir: __dirname,
+        tsConfig: '../tsconfig.json'
+      }
+    },
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-docs/register',
+    '@storybook/addon-jest/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport/register'
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(stories|story)\.[tj]sx?$/,
+      loader: require.resolve('@storybook/source-loader'),
+      exclude: [/node_modules/],
+      enforce: 'pre'
+    });
+
+    return config;
+  }
+};

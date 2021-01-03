214f8cc202b005fb22f5f8998e94e55c4db46a39
Daniel A.C. Martin <github@daniel-martin.co.uk>
2021-01-03 16:56:00 +0000

    UI: Add page titles

--- apps/ui/src/common/pages/accessibility-statement.tsx
+++ apps/ui/src/common/pages/accessibility-statement.tsx
@@ -1,11 +1,18 @@
 import { FC, Fragment, createElement as h } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { PageProps } from '@not-govuk/app-composer';
 
 import Markdown from '../../../accessibility-statement.md';
 
+export const title = 'Accessibility Statement';
+
 const Page: FC<PageProps> = props => (
-  <Markdown />
+  <Fragment>
+    <Helmet>
+      <title>{title} - LEV</title>
+    </Helmet>
+    <Markdown />
+  </Fragment>
 );
 
 export default Page;
-export const title = 'Accessibility Statement';

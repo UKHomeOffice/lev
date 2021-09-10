9ccb9c61a39aa05908ea87189483e8e414b367c5
haggishunt56 <whatevermaan@hotmail.com>
2021-09-10 14:36:16 +0100

    replace var types in dateInput with const

--- components/date-input/src/DateInput.tsx
+++ components/date-input/src/DateInput.tsx
@@ -49,22 +49,19 @@ DateInput.format = (v: string): string => {
   const yearLastDateRegEx = /^(([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])(-|\/|.)([1-9]|0[1-9]|1[0-2])(-|\/|.)(2009|201[0-9]|202[0-9]))$/g;
   const yearFirstDateRegEx = /^((2009|201[0-9]|202[0-9])-([1-9]|0[1-9]|1[0-2])-([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]))$/g;
 
-  var day, unwanted1, month, unwanted2, year
-
-  if (yearLastDateRegEx.test(v)) {
-     [ day, unwanted1, month, unwanted2, year ] = v.split(validDelimiters);
-  } else if (yearFirstDateRegEx.test(v)) {
-      [ day, unwanted1, month, unwanted2, year ] = v.split(validDelimiters).reverse();
-  } else {
-    [ day, unwanted1, month, unwanted2, year ] =
-      [
-        v.slice(0, 2),
-        null,
-        v.slice(2, 4),
-        null,
-        v.slice(4)
-      ]
-  }
+  const [ day, unwanted1, month, unwanted2, year ] = (
+    yearLastDateRegEx.test(v)
+      ? v.split(validDelimiters)
+      : yearFirstDateRegEx.test(v)
+        ? v.split(validDelimiters).reverse()
+        : [
+            v.slice(0, 2),
+            null,
+            v.slice(2, 4),
+            null,
+            v.slice(4)
+          ]
+  )
 
   if (isSet(day) && isSet(month) && isSet(year)) {
     const dd = pad(2, day);

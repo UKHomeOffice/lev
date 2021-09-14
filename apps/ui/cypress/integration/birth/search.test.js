55c1140394c5ae8fdb09c6f6edc1a202c0a5af6c
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2021-09-14 15:41:54 +0100

    test: cypress config
    including config to stop autogeneration of directories
    initial tests written that are passing

---
+++ apps/ui/cypress/integration/birth/search.test.js
@@ -0,0 +1,25 @@
+describe("birth search", () => {
+  beforeEach(() => {
+    cy.visit("/birth");
+  })
+  it("can access text in page with a search", () => {
+    cy.get('h1').should('have.text', 'Births')
+  });
+  describe('search using system number', () => {
+    it('valid record search pressing enter', () => {
+      cy.get('#system-number-input')
+        .type('123456789')
+        .type('{enter}')
+        .get('.govuk-heading-l')
+        .contains('Joan Narcissus Ouroboros SMITH 08/08/2008')
+    })
+    it('valid record search using submit button', () => {
+      cy.get('#system-number-input')
+        .type('123456789')
+        .get('.govuk-button').eq(1)
+        .click()
+        .get('.govuk-heading-l')
+        .contains('Joan Narcissus Ouroboros SMITH 08/08/2008')
+    })
+  })
+});

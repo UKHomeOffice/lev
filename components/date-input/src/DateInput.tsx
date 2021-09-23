180ed0bc452481e938b733fe3e05840a0f27070c
haggishun56 <whateermaan@htmail.com>
2021-09-23 11:52:58 +0100

    Refactor DateInput component
    Have amended DateInput component to use Luxon library to check validity of date entered. Code is more robust and allows additional formats of dates to be entered. Currently dates are only accepted when the user enters a date with delimiters.

--- components/date-input/src/DateInput.tsx
+++ components/date-input/src/DateInput.tsx
@@ -4,6 +4,8 @@ import { RawField, TextInput } from '@not-govuk/components';
 
 import '../assets/DateInput.scss';
 
+const { DateTime } = require('luxon');
+
 export type DateInputProps = StandardProps & ComponentProps<typeof TextInput>;
 
 interface WithDeformat<T> {
@@ -45,30 +47,18 @@ DateInput.format = (v: string): string => {
     !!(v || v === 0)
   );
 
-  const validDelimiters = /(-|\/|\.)/;
-  const yearLastDateRegEx = /^(([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1])(-|\/|.)([1-9]|0[1-9]|1[0-2])(-|\/|.)(2009|201[0-9]|202[0-9]))$/g;
-  const yearFirstDateRegEx = /^((2009|201[0-9]|202[0-9])-([1-9]|0[1-9]|1[0-2])-([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]))$/g;
+  const match = /^(\d{1,4})\D?(\d{1,2})\D?(\d{1,4})$/.exec(v.trim())
+  console.log(match);
+  const d = (match && (match[1].length>2 || parseInt(match[1], 10)>31))
+    ? DateTime.fromFormat(`${match[1]} ${match[2]} ${match[3]}`, 'yyyy MM dd')
+    : DateTime.fromFormat(`${match[1]} ${match[2]} ${match[3]}`, 'dd MM yyyy')
 
-  const [ day, unwanted1, month, unwanted2, year ] = (
-    yearLastDateRegEx.test(v)
-      ? v.split(validDelimiters)
-      : yearFirstDateRegEx.test(v)
-        ? v.split(validDelimiters).reverse()
-        : [
-            v.slice(0, 2),
-            null,
-            v.slice(2, 4),
-            null,
-            v.slice(4)
-          ]
-  )
+  if (d.isValid) {
+    const day = d.toFormat('dd');
+    const month = d.toFormat('MM');
+    const year = d.toFormat('yyyy');
 
-  if (isSet(day) && isSet(month) && isSet(year)) {
-    const dd = pad(2, day);
-    const mm = pad(2, month);
-    const yyyy = padYear(year);
-
-    return `${yyyy}-${mm}-${dd}`;
+    return `${year}-${month}-${day}`;
   } else {
     return undefined;
   }

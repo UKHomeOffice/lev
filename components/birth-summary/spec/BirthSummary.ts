75f3b565f6b419a8b62214a2e2e53c3bfed1ad6a
haggishun56 <whateermaan@htmail.com>
2021-10-01 15:59:55 +0100

    Amend Birth Summary unit tests
    Have added a test to ensure that the element contains a single <table> element, and have combined similar tests into a single function.

--- components/birth-summary/spec/BirthSummary.ts
+++ components/birth-summary/spec/BirthSummary.ts
@@ -23,16 +23,29 @@ describe('BirthSummary', () => {
       number: 1
     }));
 
-    it('retains the correct birthplace', () => expect(component.text()).toContain('Place of birth' + birthplace));
-    it('retains the correct birthplace', () => expect(component.text()).not.toContain('Paisley'));
-    it('retains the correct forename', () => expect(component.text()).toContain(forenames));
-    it('retains the correct forename', () => expect(component.text()).not.toContain('Fathiya'));
-    it('retains the correct surname', () => expect(component.text()).toContain(surname));
-    it('retains the correct surname', () => expect(component.text()).not.toContain('MOHAMMED'));
-    it('retains the father\'s details correctly', () => expect(component.text()).toContain('Father' + fathersName));
-    it('retains the father\'s details correctly', () => expect(component.text()).not.toContain('FatherShoaib MOHAMMED'));
-    it('retains the mother\'s details correctly', () => expect(component.text()).toContain('Mother' + mothersName));
-    it('retains the mother\'s details correctly', () => expect(component.text()).not.toContain('MotherNaia MOHAMMED'));
+    it('retains the correct birthplace', () => {
+      expect(component.text()).toContain('Place of birth' + birthplace);
+      expect(component.text()).not.toContain('Paisley');
+    });
+    it('retains the correct forename', () => {
+      expect(component.text()).toContain(forenames);
+      expect(component.text()).not.toContain('Fathiya');
+    });
+    it('retains the correct surname', () => {
+      expect(component.text()).toContain(surname);
+      expect(component.text()).not.toContain('MOHAMMED');
+    });
+    it('retains the father\'s details correctly', () => {
+      expect(component.text()).toContain('Father' + fathersName);
+      expect(component.text()).not.toContain('FatherShoaib MOHAMMED');
+    });
+    it('retains the mother\'s details correctly', () => {
+      expect(component.text()).toContain('Mother' + mothersName);
+      expect(component.text()).not.toContain('MotherNaia MOHAMMED');
+    });
+    it('contains a single <table> element', () => {
+      expect(component.find('table')).toHaveLength(1);
+    });
   });
 
   describe('when given alternative valid props', () => {
@@ -55,15 +68,28 @@ describe('BirthSummary', () => {
       number: 1
     }));
 
-    it('retains the correct birthplace', () => expect(component.text()).toContain('Place of birth' + birthplace));
-    it('retains the correct birthplace', () => expect(component.text()).not.toContain('Bournemouth'));
-    it('retains the correct forename', () => expect(component.text()).toContain(forenames));
-    it('retains the correct forename', () => expect(component.text()).not.toContain('John'));
-    it('retains the correct surname', () => expect(component.text()).toContain(surname));
-    it('retains the correct surname', () => expect(component.text()).not.toContain('SMITH'));
-    it('retains the father\'s details correctly', () => expect(component.text()).toContain('Father' + fathersName));
-    it('retains the father\'s details correctly', () => expect(component.text()).not.toContain('FatherWilliam SMITH'));
-    it('retains the mother\'s details correctly', () => expect(component.text()).toContain('Mother' + mothersName));
-    it('retains the mother\'s details correctly', () => expect(component.text()).not.toContain('MotherJane SMITH'));
+    it('retains the correct birthplace', () => {
+      expect(component.text()).toContain('Place of birth' + birthplace);
+      expect(component.text()).not.toContain('Bournemouth');
+    });
+    it('retains the correct forename', () => {
+      expect(component.text()).toContain(forenames);
+      expect(component.text()).not.toContain('John');
+    });
+    it('retains the correct surname', () => {
+      expect(component.text()).toContain(surname);
+      expect(component.text()).not.toContain('SMITH');
+    });
+    it('retains the father\'s details correctly', () => {
+      expect(component.text()).toContain('Father' + fathersName);
+      expect(component.text()).not.toContain('FatherWilliam SMITH');
+    });
+    it('retains the mother\'s details correctly', () => {
+      expect(component.text()).toContain('Mother' + mothersName);
+      expect(component.text()).not.toContain('MotherJane SMITH');
+    });
+    it('contains a single <table> element', () => {
+      expect(component.find('table')).toHaveLength(1);
+    });
   });
 });
